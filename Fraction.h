4123616ccdeb5baafd2039a95cff955702a9ab52
Eric Torunski <torunse@algonquincollege.com>
2021-02-02 19:40:45 -0500

    FInished week 4

---
+++ Fraction.h
@@ -0,0 +1,26 @@
+
+//header guard, only compile once
+
+#pragma once
+
+class Fraction {
+	int numerator, denominator;
+
+	const static int num = 0; //initialize here needs const
+
+public:
+	Fraction();//chaining to other
+	//can have  1, or 2 parameters
+	Fraction(int, int = 1); // d = 1
+	Fraction(Fraction&);
+	Fraction(Fraction* );
+
+	void setNumerator(int);
+	int getNumerator(void);
+
+	void setDenominator(int);
+	int getDenominator(void);
+
+	float asFloat(void);
+};
+
